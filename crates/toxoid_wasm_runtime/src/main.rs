e6e8b24adf347af173536879c871ff38df24a6ec
troyedwardsjr <troyedwardsjr@gmail.com>
2024-09-19 05:07:45 -0400

    create proxy host resource

--- crates/toxoid_wasm_runtime/src/main.rs
+++ crates/toxoid_wasm_runtime/src/main.rs
@@ -8,24 +8,29 @@ bindgen!({
     world: "toxoid-component-world",
     path: "../toxoid_wasm_component/wit",
     with: {
-        // Specify that our host resource is going to point to the `MyComponent`
+        // Specify that our host resource is going to point to the `ComponentProxy`
         // which is defined just below this macro.
-        "toxoid-component:component/ecs/component": MyComponent,
+        "toxoid-component:component/ecs/component": ComponentProxy,
     },
 });
 
 use exports::toxoid::api::ecs::ComponentDesc;
-use wasmtime::component::{bindgen, Component, Linker, Resource, ResourceTable};
+use wasmtime::component::{bindgen, Component, Linker, Resource, ResourceAny, ResourceTable};
 use wasmtime::{Engine, Result, Store};
 use wasmtime_wasi::{WasiCtx, WasiView, WasiCtxBuilder};
 use once_cell::sync::Lazy;
+use std::sync::{Arc, Mutex};
 
+// ResourceAnyPtr is a wrapper around ResourceAny to allow for safe concurrent access.
+struct ResourceAnyPtr(*mut ResourceAny);
+unsafe impl Send for ResourceAnyPtr {}
 /// A sample host-defined type which contains arbitrary host-defined data.
 ///
 /// In this case this is relatively simple but there's no restrictions on what
 /// this type can hold other than that it must be `'static + Send`.
-pub struct MyComponent {
+pub struct ComponentProxy {
     id: u64,
+    ptr: Arc<Mutex<ResourceAnyPtr>>
 }
 
 // StoreState is the state of the WASM store.
@@ -44,7 +49,7 @@ impl WasiView for StoreState {
 impl toxoid_component::component::ecs::Host for StoreState {}
 
 impl toxoid_component::component::ecs::HostComponent for StoreState {
-    fn new(&mut self, _desc: toxoid_component::component::ecs::ComponentDesc) -> Resource<MyComponent> {
+    fn new(&mut self, _desc: toxoid_component::component::ecs::ComponentDesc) -> Resource<ComponentProxy> {
         let engine = &*ENGINE; // Ensure ENGINE is initialized
         let linker = &*LINKER; // Ensure LINKER is initialized
         let store = unsafe { &mut *STORE }; // Ensure STORE is initialized
@@ -66,16 +71,23 @@ impl toxoid_component::component::ecs::HostComponent for StoreState {
             .unwrap();
 
         let component_id = toxoid_ecs_component.call_get_id(&mut *store, component).unwrap();
-
+        
+        let boxed_component = Box::new(component);
+        let leaked_component = Box::into_raw(boxed_component);
         let id = self
             .table
-            .push::<MyComponent>(MyComponent { id: component_id })
+            .push::<ComponentProxy>(ComponentProxy { 
+                id: component_id, 
+                ptr: Arc::new(Mutex::new(ResourceAnyPtr(leaked_component))) 
+            })
             .unwrap();
         id
     }
 
     fn get_id(&mut self, component: Resource<toxoid_component::component::ecs::Component>) -> u64 {
         let component = self.table.get(&component).unwrap();
+        println!("ID: {:?}", component.id);
+        println!("PTR: {:?}", component.ptr.lock().unwrap().0);
         component.id
     }
 
