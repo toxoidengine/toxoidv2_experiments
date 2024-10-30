0263aaba8560282bb60bdc297de86997c04659d1
troyedwardsjr <troyedwardsjr@gmail.com>
2024-10-30 19:11:27 -0400

    remove crates_old

--- crates_old/native_component/src/main.rs
+++
@@ -1,9 +0,0 @@
-use toxoid_api::bindings::exports::toxoid::api::ecs::GuestComponent;
-fn main() {
-    let component = toxoid_api::Component::new(toxoid_api::bindings::exports::toxoid::api::ecs::ComponentDesc {
-        name: "test".to_string(),
-        member_names: vec!["test".to_string()],
-        member_types: vec![0],
-    });
-    println!("{:?}", component.get_id());
-}
